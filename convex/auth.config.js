c0dc5a24246cbd020dda18a90c9d344f7b9fbd08
Nearhxh <nearhxh@gmail.com>
2024-02-19 01:04:22 +0700

    configure convex for backend & clerk for authentication

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://mutual-marten-88.clerk.accounts.dev", // get from dashboard clerk > JWT issuer
+      applicationID: "convex",
+    },
+  ],
+};
